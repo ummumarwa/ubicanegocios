185a63ee88590e6f74b5db266cfbe8553b7a54c6
kristiandamian <kristiandamian@8ba1f45a-800b-11dd-ac6e-7909132840ff>
2008-10-10 00:09:19 +0000

    A small and simple but funtional ABC

---
+++ trunk/ ubicanegocios --username kristiandamian/abc.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import cgi
+import wsgiref.handlers
+import os
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+
+from modelos import idiomas
+
+class abc (webapp.RequestHandler):    
+    def get(self):
+        """Muestro el ABC de los objetos sencillos"""
+        path = os.path.join(os.path.dirname(__file__), 'templates/abc.html')
+        listaidiomas = {
+            'listaidiomas':idiomas.all(),
+        }
+        self.response.out.write(template.render(path,listaidiomas))
--- trunk/ ubicanegocios --username kristiandamian/ubicanegocios.py
+++ trunk/ ubicanegocios --username kristiandamian/ubicanegocios.py
@@ -3,6 +3,8 @@ import wsgiref.handlers
 
 from MainPage import MainPage
 from alta import alta
+from altaajax import altaajax, bajaajax
+from abc import abc
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
@@ -10,7 +12,10 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 #Defino  la pantalla principal dentro de la clase MainPage
 application = webapp.WSGIApplication([
         ('/', MainPage),
-        ('/alta',alta)], debug=True)
+        ('/alta',alta),
+        ('/altaajax',altaajax),
+        ('/bajaajax',bajaajax),
+        ('/abc',abc)],debug=True)
 
 
 def main():
