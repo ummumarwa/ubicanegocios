bb93dcc97b9062b2e6ff2bc4ae125517a98f67eb
kristiandamian <kristiandamian@8ba1f45a-800b-11dd-ac6e-7909132840ff>
2008-10-14 15:33:00 +0000

    adding ajax & json query in abc.html

---  ubicanegocios --username kristiandamian/alta.py
+++  ubicanegocios --username kristiandamian/alta.py
@@ -5,14 +5,16 @@ import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
-from modelos import lugares
+from modelos import lugares, tipos_lugares
 
 class alta (webapp.RequestHandler):    
     def get(self):
         """Muestro la pantalla para altas de marcas"""
         marcas=lugares.all()
+        tipos=tipos_lugares.all()
         template_values = {
             'lista_lugares': marcas,
+            'tipos':tipos,
         }
         path = os.path.join(os.path.dirname(__file__), 'templates/alta.html')
         self.response.out.write(template.render(path,template_values))
---  ubicanegocios --username kristiandamian/ubicanegocios.py
+++  ubicanegocios --username kristiandamian/ubicanegocios.py
@@ -3,7 +3,7 @@ import wsgiref.handlers
 
 from MainPage import MainPage
 from alta import alta
-from altaajax import altaajax, bajaajax
+from altaajax import altaajax, bajaajax, objAjax
 from abc import abc
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
@@ -15,11 +15,12 @@ application = webapp.WSGIApplication([
         ('/alta',alta),
         ('/altaajax',altaajax),
         ('/bajaajax',bajaajax),
+        ('/objAjax',objAjax),
         ('/abc',abc)],debug=True)
 
 
 def main():
-  run_wsgi_app(application)
+    run_wsgi_app(application)
 
 #si lo corro como funcion principal que hago? Creo una instancia de application
 if __name__ == '__main__':
